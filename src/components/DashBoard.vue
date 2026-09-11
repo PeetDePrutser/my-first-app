e579f957db03c6a595864cef0c20542e4f70b024
peter <peetdeprutser@gmail.com>
2026-09-11 15:18:09 +0200

    dashboard

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -8,25 +8,25 @@
             <v-col class="w-50">
                 <v-card to="/inspections/open"><template v-slot:text>
                     <v-icon size="128">mdi-bookmark-outline</v-icon>
-                    Scheduled
+                    <p>Scheduled</p>
                 </template></v-card>
             </v-col>
             <v-col class="w-50">
                 <v-card to="/inspections/done"><template v-slot:text>
                     <v-icon size="128">mdi-check-circle</v-icon>
-                    Complete
+                    <p>Complete</p>
                 </template></v-card>
             </v-col>
             <v-col class="w-50">
                 <v-card><template v-slot:text>
                     <v-icon size="128">mdi-apps</v-icon>
-                    Knowledge base
+                    <p>Knowledge base</p>
                 </template></v-card>
             </v-col>
             <v-col class="w-50">
                 <v-card><template v-slot:text>
                     <v-icon size="128">mdi-cog</v-icon>
-                    Settings
+                    <p>Settings</p>
                 </template></v-card>
             </v-col>
         </v-row>
